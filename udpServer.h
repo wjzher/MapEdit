6ecf81b7a7f09ee88e4322fcd298a3aa40d24da5
wjzhe <wjzher@gmail.com>
2017-02-16 14:10:11 +0800

    基本实现网络模块
    Signed-off-by: wjzhe <wjzher@gmail.com>

--- udpServer.h
+++ udpServer.h
@@ -11,9 +11,19 @@
 class UdpServer : public QObject
 {
     Q_OBJECT
+    Q_PROPERTY(QString currentIp READ currentIp WRITE setCurrentIp)
 public:
     explicit UdpServer(QObject *parent = 0);
     ~UdpServer();
+    QString currentIp() const
+    {
+        return m_currentIp;
+    }
+    void setCurrentIp(const QString &s)
+    {
+        m_currentIp = s;
+    }
+    Q_INVOKABLE void SendCommand(int inf, QString &cmd);
 private:
     QString str2ip(QString s)
     {
@@ -44,15 +54,18 @@ private:
     void addAddressList(QString addr);
     bool isAddressExist(QString addr);
 
+    QByteArray makeJsonResponse(int inf, QString &param);
     int jsonMessageParse(QByteArray msg, QJsonObject &jsonObj);
     QByteArray makeTickResponse(QJsonObject &jsonObj);
+    void emitSignals(QString addr, int inf, QJsonObject &param);
 signals:
-
+    void agvStatusChanged(int inf, const QString &status);
 public slots:
     void readPendingDatagrams();
 private:
     QUdpSocket udpSocket;
     QStringList clientList;
+    QString m_currentIp;
 };
 
 #endif // UDPSERVER_H
